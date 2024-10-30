542b1ff7ba0318e0d9b829113cf88db477963e92
harry-hdh <hdh997@outlook.com>
2024-10-31 00:36:08 +1300

    Added skeleton SpecFlow project for automation testing

---
+++ Testing_Project_Mars_SpecFlow/Utilities/CommonWebDriver.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+
+
+namespace Testing_Project_Mars_SpecFlow.Utilities
+{
+    public class CommonWebDriver
+    {
+        public static IWebDriver driver;
+    }
+}
---
+++ Testing_Project_Mars_SpecFlow/Utilities/CustomMethods.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+
+
+namespace Testing_Project_Mars_SpecFlow.Utilities
+{
+    internal class CustomMethods
+    {
+        public static void Click(IWebDriver driver, By locator)
+        {
+            driver.FindElement(locator).Click();
+        }
+
+        public static void Submit(IWebDriver driver, By locator)
+        {
+            driver.FindElement(locator).Submit();
+        }
+
+        public static void Clear(IWebDriver driver, By locator)
+        {
+            driver.FindElement(locator).Clear();
+        }
+
+        public static void ClearEnterText(IWebDriver driver, By locator, string text)
+        {
+            driver.FindElement(locator).Clear();
+            //driver.FindElement(locator).Click();
+            driver.FindElement(locator).SendKeys(text);
+        }
+
+        public static void OnlyEnterText(IWebDriver driver, By locator, string text)
+        {
+            driver.FindElement(locator).SendKeys(text);
+        }
+
+
+        public static void AcceptAlert(IWebDriver driver)
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept();
+        }
+    }
+}
